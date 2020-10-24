8d3c4ec1790b0ab74aabdb5bec5df4e72e8c216e
TimoThans33 <timo.thans@me.com>
2020-10-24 10:15:50 +0200

    merging

--- Path-simulation/plot-path.py
+++ Path-simulation/plot-path.py
@@ -20,12 +20,13 @@ velocity = data["velocity"]
 x_robot = data["x_out"]
 y_robot = data["y_out"]
 plt.plot(data["x_out"], data["y_out"])
+
+kernel = np.array([[WIDTH, 0],[-WIDTH, 0],[WIDTH, HEIGH], [-WIDTH, HEIGHT]])
+rot_mat = np.array([np.cos(angle), -np.sin(angle)],[np.sin(angle), np.cos(angle)])
+
 for i in range(len(x_robot)):
-    x_kernel = [np.cos(angle[i])*WIDTH+x_robot[i]+WIDTH, -np.cos(angle[i])*WIDTH + x_robot[i] - WIDTH,
-                -np.cos(angle[i])*HEIGHT + x_robot[i] + WIDTH, np.cos(angle[i])*HEIGHT + x_robot[i] - WIDTH]
-    y_kernel = [np.sin(angle[i])*WIDTH + y_robot[i], -np.sin(angle[i])*WIDTH + y_robot[i], 
-                np.sin(angle[i])*HEIGHT + y_robot[i]+HEIGHT,-np.sin(angle[i])*HEIGHT + y_robot[i] + HEIGHT]
-    plt.plot(x_kernel, y_kernel)
+    kernel = np.matmul(kernel, rot_mat) + [x_robot[i], y_robot[i]]
+    plt.plot(kernel[0], kernel[1])
     plt.show()
 
 
